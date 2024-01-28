7f77c548190a500ae6009522a2d2ac454d4919f4
Tsiontsi <tsiontsi17@gmail.com>
2024-01-28 10:59:21 -0800

    axios,request & env

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -9,5 +9,6 @@ const requests = {
     fetchRomansMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
     fetchTvShows: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
-  }
+  };
+  
   export default requests;
